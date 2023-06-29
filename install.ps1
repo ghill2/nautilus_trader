4882a727637946fa81632a413c7bc4a8fe39be0d
ghill2 <georgehill010@gmail.com>
2023-06-29 13:04:29 +0100

    06/29/2023 13:04:29

--- install.ps1
+++ install.ps1
@@ -1,10 +1,7 @@
-$PARENT = Split-Path $MyInvocation.MyCommand.Path
-$GPARENT = Split-Path (Split-Path $MyInvocation.MyCommand.Path)
 
-# if (Get-Command -Name "deactivate" -ErrorAction SilentlyContinue) {}
 
-$NAUTILUS = Join-Path $GPARENT "nautilus_trader"
-$PYTOWER = Join-Path $GPARENT "pytower"
+$NAUTILUS = Join-Path (Split-Path (Split-Path $PSScriptRoot)) "nautilus_trader"
+$PYTOWER = Join-Path (Split-Path (Split-Path $PSScriptRoot)) "pytower"
 
 try {
     & (Join-Path $NAUTILUS ".venv/Scripts/deactivate") 2>$null
@@ -15,15 +12,10 @@ catch {
 
 Remove-Item -Path (Join-Path $NAUTILUS ".venv") -Recurse -Force
 
-# Set-Location (Join-Path $GPARENT "nautilus_trader");
 poetry install --with dev,test --all-extras
 
-
-# Set-Location (Join-Path $GPARENT "pytower");
-
-# execute the script within the current PowerShell session
-# & ~/BU/projects/nautilus_trader/.venv/Scripts/activate.ps1
 & (Join-Path $NAUTILUS ".venv/Scripts/activate.ps1")
+
 pip install -r (Join-Path $PYTOWER "requirements.txt")
 
 pip install (Join-Path $PYTOWER "TA_Lib-0.4.24-cp310-cp310-win_amd64.whl")
